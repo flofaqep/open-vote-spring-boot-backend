6a881228e638033d1b7a563c83d0743ba36836c5
fuavnook <31309354+fuavnook@users.noreply.github.com>
2017-08-24 14:58:10 +0200

    Update README.md
    Add .gitignore
    
    Initial commit with Spring initializr project
    
    Fix pom.xml version and indentation

---
+++ src/main/java/org/truesight/openvote2018/OpenVote2018Application.java
@@ -0,0 +1,12 @@
+package org.truesight.openvote2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenVote2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenVote2018Application.class, args);
+	}
+}
